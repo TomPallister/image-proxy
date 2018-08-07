79b686b06e3e0f7a056c618edff94104b423a281
Tom Gardham-Pallister <thomas.gardham-pallister@news.co.uk>
2018-08-07 18:58:18 +0530

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace image_proxy {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseDeveloperExceptionPage ();
+            app.Run (async context => {
+                var url = context.Request.Query.First (x => x.Key == "url");
+                using (var httpClient = new HttpClient ()) {
+                    var response = await httpClient.GetAsync (url.Value);
+                    var content = await response.Content.ReadAsStreamAsync ();
+                    using (content) {
+                        await content.CopyToAsync (context.Response.Body);
+                    }
+                }
+            });
+        }
+    }
+}
