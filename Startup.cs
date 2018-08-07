8670b30d524b2fb02629b3985c7ece7c70db661d
Tom Gardham-Pallister <thomas.gardham-pallister@news.co.uk>
2018-08-07 19:13:10 +0530

    enabled cors

--- Startup.cs
+++ Startup.cs
@@ -24,10 +24,16 @@ namespace image_proxy {
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
             services.AddMvc ().SetCompatibilityVersion (CompatibilityVersion.Version_2_1);
+            services.AddCors (o => o.AddPolicy ("MyPolicy", builder => {
+                builder.AllowAnyOrigin ()
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ();
+            }));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseCors("MyPolicy");
             app.UseDeveloperExceptionPage ();
             app.Run (async context => {
                 var url = context.Request.Query.First (x => x.Key == "url");
